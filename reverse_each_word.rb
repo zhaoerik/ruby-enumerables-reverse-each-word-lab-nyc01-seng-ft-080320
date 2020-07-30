95305892421752b571f5d22a2b49c6cfab288b08
Erik Zhao <erikzhao98@gmail.com>
2020-07-30 19:12:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 require 'pry'
 
 def reverse_each_word(string)
-  string = string.split
-  array = []
-  binding.pry
-  string.each do |i| array.unshift[i]
+  string.each do |word|
+    word.reverse
   end
 end
